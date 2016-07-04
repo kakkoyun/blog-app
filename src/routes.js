2ed6da34ca7a2e08f1e1dad69e31ca732e53674a
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 19:27:36 +0300

    Add initial routes and experiment.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostIndex from './components/post_index';
+
+const Greeting = () => {
+  return <div>Hey baby!</div>;
+};
 
 export default (
-  <Route path="/" component={App}/>
+  <Route path="/" component={App}>
+    <IndexRoute component={PostIndex}/>
+    <Route path="greet" component={Greeting}/>
+  </Route>
 );
