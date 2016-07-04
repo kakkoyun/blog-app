a503b2d9598643953da79c965cf883348c381f13
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 21:38:03 +0300

    Implement first simple validation for form.

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -15,6 +15,9 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
         <div className="form-group">
           <label>Categories</label>
@@ -30,11 +33,20 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Enter Username';
+  }
+  return errors;
+}
+
 // connect: first argument is mapStateToProps, second is mapDispatchToProps
 // reduxForm: first argument is config object, second is mapStateToProps, third is mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
 
 // same as;
