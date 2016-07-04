144c2eac0cac5d8f0afa3764dd649b1d6785e388
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 19:51:13 +0300

    Convert post list to a container and connect fetch post action.

--- src/components/post_index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return <div>List of Posts</div>;
-}
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import PostIndex from './components/post_index';
+import PostIndex from './containers/post_index';
 
 export default (
   <Route path="/" component={App}>
