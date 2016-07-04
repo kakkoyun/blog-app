da4dd796921ff9d041212bf396504e6b1411a687
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 21:03:31 +0300

    Create post form with redux form.

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
   render() {
+    // Redux-form to handle submit of form.
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create new Post</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create a new Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title}/>
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories}/>
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content}/>
+        </div>
+        <button type="submit" className="btn btn-primary">Create</button>
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
