13b37b3e9d83fd973d6a8e8fa7a64bb56b452c7d
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 21:13:56 +0300

    Implement submit for post create. Add related action and APi request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import  axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asdasdaslkklmdioewincmzxcxmnznasd';
@@ -12,4 +13,13 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props); // axios is handy!
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
+import { createPost } from '../actions/index';
+
 class PostsNew extends Component {
 
   render() {
@@ -8,7 +10,7 @@ class PostsNew extends Component {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -28,7 +30,16 @@ class PostsNew extends Component {
   }
 }
 
+// connect: first argument is mapStateToProps, second is mapDispatchToProps
+// reduxForm: first argument is config object, second is mapStateToProps, third is mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
+
+// same as;
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ cratePost }, dispatch);
+// }
+// ...
+// }, null, mapDispatchToProps)(PostsNew);
