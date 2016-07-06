1f4970ad5d8113e97bd9f97a290537e4d5f1ea50
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-06 20:32:21 +0300

    Implement navigation after successful post.

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // Blog post has been created navigate user to index.
+        // Meeeh :(
+        this.context.router.push('/');
+      })
+  }
 
   render() {
     // Redux-form to handle submit of form.
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a new Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : '' }`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
           <div className="text-help">
             {title.touched ? title.error : ''}
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : '' }`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : '' }`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
         <button type="submit" className="btn btn-primary">Create</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -36,7 +57,13 @@ class PostsNew extends Component {
 function validate(values) {
   const errors = {};
   if (!values.title) {
-    errors.title = 'Enter Username';
+    errors.title = 'Enter Title';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some Categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some Content';
   }
   return errors;
 }
